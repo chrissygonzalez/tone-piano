4e5abe460a25aacabfe9867ba1b78dc3a1626094
chrissygonzalez <chrissygonzalez@gmail.com>
2019-11-06 11:48:53 -0500

    start basic PianoContainer

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Test from './test';
 import Tone from 'tone';
+import PianoContainer from './containers/PianoContainer';
 
-function App() {
-  Test.getSongs();
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  playSong = () => {
+    Tone.Transport.stop();
+    Tone.Transport.cancel(0);
+
+    let synth = new Tone.Synth().toMaster();
+    let counter = 0;
+    new Tone.Part(function(time, value){
+      Tone.Draw.schedule(function(){
+        // gross temporary animation
+        let notes = document.getElementsByClassName('song');
+        let notesArr = Array.from(notes);
+        notesArr.map(note => note.style.backgroundColor = '#fff');
+
+        let note = document.getElementById(counter)
+        note.style.backgroundColor = 'yellow';
+        counter += 1;
+      }, time)
+
+      synth.triggerAttackRelease(value.note, "8n", time, value.velocity);
+    }, this.state.song).start(0);
+
+    Tone.Transport.start("+0.1");
+  }
+
+  render() {
+    Test.getSongs();
+    const inlineBlock = {display: 'inline-block', padding: '5px'}
+
+    return (
+      <div className="App">
+        <button onClick={this.playSong}>Play Song</button>
+        <div>{this.state.song.map((note, index) => <div key={index} id={index} className="song" style={inlineBlock}>{note.note}</div>)}</div>
+        <PianoContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
