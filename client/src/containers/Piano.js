f34fae09977e0f73313619fb70c053d575fb2d7e
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2019-11-18 22:01:21 -0500

    move piano keyboard listener

--- client/src/containers/Piano.js
+++ client/src/containers/Piano.js
@@ -2,23 +2,17 @@ import React from 'react';
 
 const Piano = (props) => {
     return (
-        <div className="piano">
-            <button onClick={() => props.saveNote('C4')} onKeyDown={e => props.handleKey(e)}>C</button>
-            <button onClick={() => props.saveNote('D4')} onKeyDown={e => props.handleKey(e)}>D</button>
-            <button onClick={() => props.saveNote('E4')} onKeyDown={e => props.handleKey(e)}>E</button>
-            <button onClick={() => props.saveNote('F4')} onKeyDown={e => props.handleKey(e)}>F</button>
-            <button onClick={() => props.saveNote('G4')} onKeyDown={e => props.handleKey(e)}>G</button>
-            <button onClick={() => props.saveNote('A4')} onKeyDown={e => props.handleKey(e)}>A</button>
-            <button onClick={() => props.saveNote('B4')} onKeyDown={e => props.handleKey(e)}>B</button>
-            <button onClick={() => props.saveNote('C5')} onKeyDown={e => props.handleKey(e)}>C</button>
+        <div className="piano" onKeyDown={e => props.handleKey(e)}>
+            <button onClick={() => props.saveNote('C4')}>C</button>
+            <button onClick={() => props.saveNote('D4')}>D</button>
+            <button onClick={() => props.saveNote('E4')}>E</button>
+            <button onClick={() => props.saveNote('F4')}>F</button>
+            <button onClick={() => props.saveNote('G4')}>G</button>
+            <button onClick={() => props.saveNote('A4')}>A</button>
+            <button onClick={() => props.saveNote('B4')}>B</button>
+            <button onClick={() => props.saveNote('C5')}>C</button>
         </div>
     )
 }
 
 export default Piano;
-
-// document.addEventListener("keydown", e => {
-//     // e object has the key property to tell which key was pressed
-//     switch (e.key) {
-//       case "d":
-//         return synth.triggerAttack("C4");
