63602da5068410ee3125e43b280eae89898bd891
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2019-12-02 16:13:35 -0500

    styling song thumbs and song form

--- client/src/components/SongForm.js
+++ client/src/components/SongForm.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 const SongForm = (props) => {
   return (
-    <form>
-        {/* <label htmlFor="title">Title</label> */}
+    <form className="song-form">
         <input name="title" type="text" onChange={ e => {props.onChange(e)}} value={props.title} placeholder="Song Title" /> by
-        {/* <label htmlFor="musician_name">Your Name</label> */}
         <input name="musician_name" type="text" onChange={ e => {props.onChange(e)}} value={props.musician_name} placeholder="Your Name" />
     </form>
   );
--- client/src/components/SongThumb.js
+++ client/src/components/SongThumb.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 const SongThumb = (props) => {
   return (
     <li key={props.song.id} className="song-thumb">
-        <Link to={`/songs/${props.song.id}`}>{props.song.title} by {props.song.musician_name}</Link>
-        <p>Notes: {props.song.notes ? props.song.notes.map(note => note.note) : null}</p>
+        <Link to={`/songs/${props.song.id}`}>
+          <span className="song-title">{props.song.title}</span>
+          <p>by {props.song.musician_name}</p>
+          </Link>
     </li>
   );
 };
