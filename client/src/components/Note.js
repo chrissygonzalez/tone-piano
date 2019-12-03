26735a418f6bdcd1e6e1fed13b6af614fb3b6689
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2019-12-03 13:21:34 -0500

    move staff background to notes

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Note = (props) => {
-    const styles = `${props.noteAppearance.style} song flex`;
+    const styles = `${props.noteAppearance.style} song flex note-single`;
         return (
         <div key={props.index} id={props.index} className={styles}>
             {props.noteAppearance.shape}
